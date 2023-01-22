f3ff5ffef4fe8f600b6a51a5f8a578d4637475d5
aylin gunes <4ylingunes@gmail.com>
2023-01-22 15:01:28 +0100

    write inheritance and polymorphism threads by creating subclasses and superclasses

--- inheritance-polymorphism.playground/Contents.swift
+++ inheritance-polymorphism.playground/Contents.swift
@@ -1,3 +1,49 @@
 import UIKit
 
 var greeting = "inheritance-polymorphism"
+
+
+class Vehicle {
+    var vehicleColor:String?
+    var vehicleName:String?
+    
+    init(vehicleColor:String,vehicleName:String) {
+        self.vehicleColor = vehicleColor
+        self.vehicleName = vehicleName
+    }
+    
+    
+    }
+
+class Car:Vehicle {
+    
+    var carEnginePower:Int?
+    var carSeat:Int?
+    
+    init(carEnginePower:Int, carSeat:Int,vehicleColor:String,vehicleName:String) {
+        self.carSeat = carSeat
+        self.carEnginePower = carEnginePower
+        super.init(vehicleColor: vehicleColor, vehicleName: vehicleName)
+    }
+    
+}
+
+class Nissan:Car {
+    var model:String?
+    // this class should include Car&Vehicle classes init values
+    
+    
+    
+    init(carEnginePower: Int, carSeat: Int, vehicleColor: String, vehicleName: String, model:String) {
+        self.model = model
+        super.init(carEnginePower: carEnginePower, carSeat: carSeat, vehicleColor: vehicleColor, vehicleName: vehicleName)
+    }
+    
+    
+}
+
+var bmw = Car(carEnginePower: 2400, carSeat: 4, vehicleColor: "black", vehicleName: "BMW 3.20")
+
+var qasqhai = Nissan(carEnginePower: 1200, carSeat: 5, vehicleColor: "purple", vehicleName: "Qashqai", model: "X234")
+
+print(qasqhai.carEnginePower!)
